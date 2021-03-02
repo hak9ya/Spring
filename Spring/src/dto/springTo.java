4d63eef19f3c6e15c0d290428334f78029f2edcd
hak9ya <jhy2297@naver.com>
2021-03-02 17:06:13 +0900

    Test)

--- Spring/src/dto/springTo.java
+++ Spring/src/dto/springTo.java
@@ -1,11 +1,19 @@
 package dto;
 
 public class springTo {
+	private int springLevel;
+	private int X;
+	private int Y;
+	
+	public springTo(int springLevel) {
+		this.springLevel = springLevel;
+		X = 0;
+		Y = 0;
+	}
 
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
 		System.out.println("hello");
-
+		
 	}
 
 }
